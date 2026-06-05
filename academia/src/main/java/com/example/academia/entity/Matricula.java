04f80cd10729530730d655d0bc7f5dd5e5408f8c
Massayoshii <Massayoshibando@gmail.com>
2026-06-05 15:08:43 -0300

    feat: criacao da entidade matricula

---
+++ academia/src/main/java/com/example/academia/entity/Matricula.java
@@ -0,0 +1,40 @@
+package com.example.academia.entity;
+
+import com.example.academia.entity.enums.StatusMatricula;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "matriculas")
+@Getter
+@Setter
+public class Matricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_matricula")
+    private LocalDate dataMatricula;
+    @Column(name = "dia_vencimento")
+    private Integer diaVencimento;
+    @Column(name = "data_encerramento")
+    private LocalDate dataEncerramento;
+
+    @Enumerated(EnumType.STRING)
+    private StatusMatricula statusMatricula = StatusMatricula.ATIVA;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    public void prePersist(){
+        if (dataMatricula == null){
+            dataMatricula = LocalDate.now();
+        }
+    }
+
+
+}
