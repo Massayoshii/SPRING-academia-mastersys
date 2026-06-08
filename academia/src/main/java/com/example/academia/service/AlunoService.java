1f953e9eae9dd768b15397a47d94635a2289e302
Massayoshii <Massayoshibando@gmail.com>
2026-06-08 20:52:12 -0300

    feat: criacao do service aluno

---
+++ academia/src/main/java/com/example/academia/service/AlunoService.java
@@ -0,0 +1,50 @@
+package com.example.academia.service;
+
+import com.example.academia.dto.AlunoRequest;
+import com.example.academia.dto.AlunoResponse;
+import com.example.academia.entity.Aluno;
+import com.example.academia.repository.AlunoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AlunoService {
+
+    public final AlunoRepository alunoRepository;
+
+    public AlunoResponse cadastrar(AlunoRequest request){
+        if (request.email() != null && alunoRepository.existsByEmail(request.email())){
+            throw new RuntimeException("Email já cadastrado");
+        }
+        Aluno aluno = request.toEntity();
+        Aluno alunoSalvo = alunoRepository.save(aluno);
+        return AlunoResponse.fromEntity(alunoSalvo);
+    }
+
+    public Page<AlunoResponse> listar(Pageable pageable){
+        return alunoRepository.findAll(pageable).map(AlunoResponse::fromEntity);
+    }
+    public AlunoResponse buscarPorId(Long id){
+        Aluno aluno = buscarEntidadePorId(id);
+        return AlunoResponse.fromEntity(aluno);
+    }
+
+    public AlunoResponse atualizar(Long id , AlunoRequest request){
+        Aluno aluno = buscarEntidadePorId(id);
+        request.preencher(aluno);
+        Aluno alunoAtualizado = alunoRepository.save(aluno);
+        return AlunoResponse.fromEntity(alunoAtualizado);
+    }
+
+    public void excluir(Long id ){
+        Aluno aluno = buscarEntidadePorId(id);
+        alunoRepository.delete(aluno);
+    }
+
+    private Aluno buscarEntidadePorId(Long id){
+        return alunoRepository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+    }
+}
