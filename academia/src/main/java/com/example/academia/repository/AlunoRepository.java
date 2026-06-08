04ee4cd53b3f7ff417d769c44684884cd9701182
Massayoshii <Massayoshibando@gmail.com>
2026-06-08 20:51:33 -0300

    refactor: adicionado metodo no repositorio aluno

--- academia/src/main/java/com/example/academia/repository/AlunoRepository.java
+++ academia/src/main/java/com/example/academia/repository/AlunoRepository.java
@@ -4,4 +4,5 @@ import com.example.academia.entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno , Long> {
+    boolean existsByEmail(String email);
 }
