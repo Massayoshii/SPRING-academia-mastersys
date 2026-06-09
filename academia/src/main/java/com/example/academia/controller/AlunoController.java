2eecd61959b6014984f8c273c4d5130481de111b
Massayoshii <Massayoshibando@gmail.com>
2026-06-09 09:48:57 -0300

    feat: criacao do controller aluno

---
+++ academia/src/main/java/com/example/academia/controller/AlunoController.java
@@ -0,0 +1,46 @@
+package com.example.academia.controller;
+
+import com.example.academia.dto.AlunoRequest;
+import com.example.academia.dto.AlunoResponse;
+import com.example.academia.service.AlunoService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+@AllArgsConstructor
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AlunoResponse cadastrar(@RequestBody AlunoRequest alunoRequest){
+        return alunoService.cadastrar(alunoRequest);
+    }
+
+    @GetMapping
+    public Page<AlunoResponse> listar(Pageable pageable){
+        return alunoService.listar(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public AlunoResponse buscarPorId(@PathVariable   Long id){
+        return alunoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AlunoResponse atualizar(@PathVariable Long id , @RequestBody AlunoRequest alunoRequest){
+        return alunoService.atualizar(id,alunoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id){
+        alunoService.excluir(id);
+    }
+
+}
