3d12400f44e5eb8d2187317ae88deda7557d3557
Massayoshii <Massayoshibando@gmail.com>
2026-06-09 11:02:18 -0300

    refactor: implementacao das exceptions no service aluno

--- academia/src/main/java/com/example/academia/service/AlunoService.java
+++ academia/src/main/java/com/example/academia/service/AlunoService.java
@@ -3,6 +3,8 @@ package com.example.academia.service;
 import com.example.academia.dto.AlunoRequest;
 import com.example.academia.dto.AlunoResponse;
 import com.example.academia.entity.Aluno;
+import com.example.academia.exceptions.AlunoNaoEncontradoException;
+import com.example.academia.exceptions.EmailJaCadastradoException;
 import com.example.academia.repository.AlunoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,7 +19,7 @@ public class AlunoService {
 
     public AlunoResponse cadastrar(AlunoRequest request){
         if (request.email() != null && alunoRepository.existsByEmail(request.email())){
-            throw new RuntimeException("Email já cadastrado");
+            throw new EmailJaCadastradoException();
         }
         Aluno aluno = request.toEntity();
         Aluno alunoSalvo = alunoRepository.save(aluno);
@@ -45,6 +47,6 @@ public class AlunoService {
     }
 
     private Aluno buscarEntidadePorId(Long id){
-        return alunoRepository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+        return alunoRepository.findById(id).orElseThrow(() -> new AlunoNaoEncontradoException(id));
     }
 }
